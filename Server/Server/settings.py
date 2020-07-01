7914706615a495635f7cd295e3fb112b7dd61519
unknown <kittisak.ma.59@ubu.ac.th>
2020-07-01 15:30:10 +0700

    Add app restapi in server

--- Server/Server/settings.py
+++ Server/Server/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
 
     # local
     'restauth',
+    'restapi',
 ]
 
 MIDDLEWARE = [
--- Server/Server/urls.py
+++ Server/Server/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^rest_auth/v1/', include('restauth.urls')),
-    # url(r'^api/v1/', include('api.urls')),
+    url(r'^rest_api/v1/', include('restapi.urls')),
 ]
